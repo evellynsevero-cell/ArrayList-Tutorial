f7649c1d81b0fdfbe72c5a3a467ccc9a73969356
evellyn severo <evellyn.severo@zup.com.br>
2026-01-05 11:05:23 -0300

    Estrutura inicial: Construtor e toString

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Inicializa um objeto 'Vetor' capaz de armazenar 5 elementos.
+        Vetor vetor = new Vetor(5);
+        /**
+         * Imprime o estado inicial do vetor no console.
+         * Espera-se a saída:
+         * Vetor {
+         *      items: []
+         *      capacity: 5
+         *      size: 0
+         * }
+         **/
+        System.out.println(vetor);
+
+    }
+}
---
+++ src/Vetor.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+
+
+/**
+ * Representa uma estrutura de dados de vetor (array)
+ * que armazena elementos do tipo String.
+ */
+public class Vetor {
+    private Integer[]elementos; // Array interno para armazenar os dados
+    private int tamanho;       // Número atual de elementos inseridos
+
+    /**
+     *  Contrutor da classe Vetor.
+     * @param capacidade 0 número máximo inicial de elementos que o vetor pode armazenar.
+     */
+    public Vetor(int capacidade) {
+        // Inicializa o array com a capacidade definida
+        this.elementos = new Integer[capacidade];
+        // 0 vetor começa vazio
+        this.tamanho = 0;
+    }
+    /**
+     * Retorna uma representação em String da estrutura.
+     * O formato inclui:
+     * * a lista de elementos inseridos (separados por vírgula),
+     * * a capacidade total do array interno, e
+     * * o tamanho logico atual.
+     * @return Uma String contendo o estado atual da estrutura.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Vetor {\n");
+        sb.append("\titems: [");
+        for (int position = 0; position < size; position++) {
+        sb.append(items[position]);
+        if (position < size - 1){
+            sb.append(", ");
+        }
+
+    }
+        sb.append("]\n");
+        sb.append(String.format("\tcapacity: %d\n", items.length));
+        sb.append(String.format("tsize: %d/n" , size));
+        sb.append("}");
+        return  sb.toString();
+}
+
+// Métodos para adicionar, buscar, remover, etc. serão implementados aqui...
